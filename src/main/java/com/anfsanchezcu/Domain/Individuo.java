91e9d5ff6f90086f58729695b96081e7e1112a45
anfsanchezcu <anfsanchezcu@unal.edu.co>
2024-04-25 14:45:22 -0500

    advance: pass valors by properties document to html xml

---
+++ src/main/java/com/anfsanchezcu/Domain/Individuo.java
@@ -0,0 +1,10 @@
+package com.anfsanchezcu.Domain;
+
+public class Individuo {
+  private String name;
+  private String lastName;
+  private String tel;
+  private String email;
+  private String age;
+  
+}
--- src/main/java/com/anfsanchezcu/HolaSpring/ControladorREST.java
+++ src/main/java/com/anfsanchezcu/HolaSpring/ControladorREST.java
@@ -1,19 +1,31 @@
 package com.anfsanchezcu.HolaSpring;
 
 import  lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+
+
+@Controller
 @Slf4j
 public class ControladorREST {
   
-  @GetMapping("/")
-  public String saludo() {
-    log.info("Estoy ejecutando el controlador rest");
-    log.info("MAS INFO");
+  @Value("${index.aux_hola}")
+  private String aux_hola;
 
-    return "Hola Spring Boot";
+
+  @GetMapping("/")
+  public String saludo(Model model) {
+    log.info("Estoy ejecutando el controlador MVC");
+
+    String hola = "Hola Spring Boot con Thymeleaf";
+    model.addAttribute("hola",hola);
+    model.addAttribute("aux_hola",aux_hola);
+    
+    return "index";
   }
 }
